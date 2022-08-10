4a11728e108dbb466846aceac2d14263d933f262
hesamchobanlou <hesam.chobanlou@gmail.com>
2022-08-10 00:11:28 -0400

    adding kinesis firehose portion

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# DynamoDB TTL Stream to S3
+# DynamoDB TTL Stream to S3 Bucket
+
+<img src="images/OverviewImage.png" />
 
 ### Create DynamoDB Table
 1. Create a table called ```test_ttl_table``` e.g,:
@@ -34,7 +36,11 @@ If you are adding the filter through the AWS Trigger Configuration page, use bel
 {"userIdentity":{"type":["Service"],"principalId":["dynamodb.amazonaws.com"]}}
 ```
 
-If you receive an error the function is missing Permissions, attach ```AWSLambdaInvocation-DynamoDB``` policy to the lambda generated role.
+Attach ```AWSLambdaInvocation-DynamoDB``` and ```AmazonKinesisFirehoseFullAccess``` policies to the lambda function role.
+
+### Create a Kinesis Data Firehose Delivery stream
+Create a new delivery stream that will place data into an S3 bucket of your choice. In this example, 
+the generated delivery stream name is ```PUT-S3-CwAUa```.
 
 ### Build Java Project
 ```mvn package```
@@ -45,3 +51,41 @@ aws lambda update-function-code --function-name test_ddb_ttl --zip-file fileb://
 ```
 
 Change ```<lambda_function.jar>``` to point to the generated .jar file in the ```/target``` directory.
+
+### Test Record w/ expdate
+Create a test record in the ```test_ttl_table``` table and wait until TTL picks up the record.
+
+CloudWatch Logs and the S3 Bucket should contain record details similar to what is below:
+```
+{
+  eventID: 70a21e0f6fc0f4e897952170595355cb,
+  eventName: REMOVE,
+  eventVersion: 1.1,
+  eventSource: aws:dynamodb,
+  awsRegion: us-east-1,
+  dynamodb: 
+  {
+    ApproximateCreationDateTime: Wed Aug 10 02:45:47 UTC 2022,
+    Keys: 
+    {
+      my_pk={S: 5fa95c3a-1854-11ed-861d-0242ac120002,}
+    },
+    OldImage: 
+    {
+      firstname={S: Reeves,}, 
+      my_pk={S: 5fa95c3a-1854-11ed-861d-0242ac120002,}, 
+      expdate={N: 1660098720,}, 
+      lastname={S: James,}
+    },
+    SequenceNumber: 20359600000000008434716639,
+    SizeBytes: 123,
+    StreamViewType: NEW_AND_OLD_IMAGES
+  },
+  userIdentity: 
+  {
+    PrincipalId: dynamodb.amazonaws.com,
+    Type: Service
+  },
+  eventSourceArn: arn:aws:dynamodb:us-east-1:************:table/test_ttl_table/stream/2022-08-09T22:16:04.930
+}
+```
--- src/main/java/example/Handler.java
+++ src/main/java/example/Handler.java
@@ -1,28 +1,74 @@
 package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
-
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
-import java.util.Map;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.firehose.FirehoseClient;
+import software.amazon.awssdk.services.firehose.model.FirehoseException;
+import software.amazon.awssdk.services.firehose.model.PutRecordRequest;
+import software.amazon.awssdk.services.firehose.model.PutRecordResponse;
+import software.amazon.awssdk.services.firehose.model.Record;
 
 // Handler value: example.Handler
-public class Handler implements RequestHandler<Map<String,String>, String>{
+public class Handler implements RequestHandler<DynamodbEvent, String>{
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
     @Override
-    public String handleRequest(Map<String,String> event, Context context)
-    {
+    public String handleRequest(DynamodbEvent dynamodbEvent, Context context) {
         LambdaLogger logger = context.getLogger();
+
         String response = "200 OK";
+
         // log execution details
         logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
         logger.log("CONTEXT: " + gson.toJson(context));
+
         // process event
-        logger.log("EVENT: " + gson.toJson(event));
-        logger.log("EVENT TYPE: " + event.getClass());
+        logger.log("EVENT: " + gson.toJson(dynamodbEvent.toString()));
+        logger.log("EVENT TYPE: " + dynamodbEvent.getClass());
+
+        // Firehose client
+        FirehoseClient firehoseClient = FirehoseClient.builder().build();
+
+        logger.log("RECORDS:...");
+        for (DynamodbEvent.DynamodbStreamRecord dRecord : dynamodbEvent.getRecords()) {
+            try {
+                // log record
+                logger.log(dRecord.toString());
+
+                // Forward record to Kinesis Firehose
+                String deliveryStreamName = "PUT-S3-CwAUa";
+
+                logger.log(String.format("Sending dynamo record with eventId: %s to delivery stream: %s", dRecord.getEventID(), deliveryStreamName));
+                SdkBytes sdkBytes = SdkBytes.fromByteArray(dRecord.toString().getBytes());
+                Record kRecord = Record.builder()
+                        .data(sdkBytes)
+                        .build();
+
+                PutRecordRequest kPutRecordRequest = PutRecordRequest.builder()
+                        .deliveryStreamName(deliveryStreamName)
+                        .record(kRecord)
+                        .build();
+
+                PutRecordResponse kRecordPutResponse = firehoseClient.putRecord(kPutRecordRequest);
+
+                logger.log(String.format("Completed sending record with record ID: %s", kRecordPutResponse.recordId()));
+            } catch (FirehoseException e) {
+                logger.log("Error occurred attempting to put record to kinesis delivery stream.");
+                logger.log(e.getLocalizedMessage());
+
+                firehoseClient.close();
+
+                return "500 ERROR";
+            }
+        }
+
+        firehoseClient.close();
+
         return response;
     }
 }
